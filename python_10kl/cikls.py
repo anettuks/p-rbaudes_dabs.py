7b740095857973fd10e32436b928a9122c7c66ff
anettuks <152157843+anettuks@users.noreply.github.com>
2024-03-25 10:15:47 +0200

    Add files via upload

---
+++ python_10kl/cikls.py
@@ -0,0 +1,51 @@
+# i = 1
+# while i<=5:
+#     print("labi")
+#     i+=1
+#     print("i tagad ir", i)
+
+# j = 0  
+# while j < 5:
+#   print("Nr.", j)
+#   j += 1
+
+
+# i = 5
+# while i > 0:
+#   print("Skaita atpakaļ", i)
+#   i -= 1
+
+# i = 20
+# while True: 
+#   if i > 26:
+#     break 
+# print("i ir", i)
+# i += 2
+
+
+# augstums = int(input("Noradi skaitu: "))
+# i = 0
+# while i < augstums:
+#     print("*" *augstums)
+#     i += 1
+
+
+# i = 1
+# while i<= 100:
+#     if i % 5 == 0 and i%7==0:
+#         print("FizzBuzz", end= " " )
+#     elif i % 7 == 0:
+#         print("Buzz ",end= " ")
+#     elif i % 5 == 0:
+#         print("Fizz ",end= " ")
+#     else:
+#         print("i ",end= " ")
+
+
+mainigais = [1,2,3] 
+for elements in mainigais:
+  print(elements)
+
+
+        
+    
