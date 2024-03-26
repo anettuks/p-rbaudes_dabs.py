66ef5ad7f9b271a742cf55c41b4f29ccfb13f533
anettuks <152157843+anettuks@users.noreply.github.com>
2024-03-26 11:53:45 +0200

    spēle.py

--- pd.py/spēle.py
+++ pd.py/spēle.py
@@ -1,15 +1,15 @@
 import random
 
 def skaitlis():
-    print("Uzmini skaitli no 1 līdz 100")
-izvelies_skaitli = random.randint(1, 100)
+    print("Uzmini skaitli no 1 līdz 100") #ši funkcja parāda, kas lietotājm jādara.
+izvelies_skaitli = random.randint(1, 100) #šī funkcija saglabā skaitļus no 1 līdz 100 tos nemainot
 
 while True:
     minejums = int(input("Ievadi minēto skaitli(no 1 līdz 100):"))
-    if minejums == izvelies_skaitli:
-        print("Apsveicu tu uzminēji skaitli!") 
-        break
-    elif minejums < izvelies_skaitli: 
+    if minejums == izvelies_skaitli: #funkcija pārbauda vai lietotājs ir uzminējis skaitli
+        print("Apsveicu tu uzminēji skaitli!") #ja spēlētājs uzminēja skaitli tad viņu apsveic
+        break #šī funkcija pārtrauc spēli, lai spēletajs varētu spēli sākt no jauna  
+    elif minejums < izvelies_skaitli:  #šī funkcija pārbauda vai minējums ir lielāks vai mazāks
         print("Minētais skaitlis ir lielāks. Meiģini velreiz.")
     else:
         print("Minētais skaitlis ir mazāks. Meiģini velreiz.")
