d8a6d7e362a9bcb47de94638f54bc569ec7d06ff
anettuks <152157843+anettuks@users.noreply.github.com>
2024-03-26 12:38:46 +0200

    spēle.py

--- pd.py/spēle.py
+++ pd.py/spēle.py
@@ -8,10 +8,18 @@ while True:
     minejums = int(input("Ievadi minēto skaitli(no 1 līdz 100):"))
     if minejums == izvelies_skaitli: #funkcija pārbauda vai lietotājs ir uzminējis skaitli
         print("Apsveicu tu uzminēji skaitli!") #ja spēlētājs uzminēja skaitli tad viņu apsveic
-        break #šī funkcija pārtrauc spēli, lai spēletajs varētu spēli sākt no jauna  
+
+        atkartot = input ("Vai vēlies spēli sākt velreiz? (jā/nē): ")
+        if atkartot.lower() == "jā":
+          skaitlis()
+        else:
+
+
+          break #šī funkcija pārtrauc spēli, lai spēletajs varētu spēli sākt no jauna  
     elif minejums < izvelies_skaitli:  #šī funkcija pārbauda vai minējums ir lielāks vai mazāks
         print("Minētais skaitlis ir lielāks. Meiģini velreiz.")
     else:
         print("Minētais skaitlis ir mazāks. Meiģini velreiz.")
 
 skaitlis()
+    
